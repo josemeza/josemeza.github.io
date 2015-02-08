b450d800162f4b244f40932b4630b2d251cc4ceb
Daniel Munro <munro.daniel@gmail.com>
2015-02-07 20:16:41 -0500

    update

--- index.js
+++ index.js
@@ -3,15 +3,28 @@ $(document).ready(function() {
 	dimmer.hide();
 	var carousel = $('.carousel');
 	carousel.hide();
+	var carouselLabel = $('#carousel-label');
+	carouselLabel.hide();
+	var carouselCaption = $('#carousel-caption');
+	carouselCaption.hide();
 
 	var z = 100;
 	var grid = $('.grid');
 	var tiles = $('.tile');
-	var msnry = new Masonry('.grid', {
-		"gutter": 16,
-		isAnimated: true
-	});
-
+	// var msnry = new Masonry('.grid', {
+	// 	"gutter": 16,
+	// 	isAnimated: true
+	// });
+$('.grid').isotope({
+	layoutMode: 'masonry',
+	masonry: {
+		gutter: 16
+	},
+	getSortData: {
+		id: '[data-id]'
+	},
+	sortBy: 'id'
+});
 
 	// var shufBack = $('#shuffle-background');
 	// shufBack.css('left', grid.position().left);
@@ -21,15 +34,30 @@ $(document).ready(function() {
 	// 	var tile = $(this);
 	// 	tile.attr('data-id', i);
 	// });
-
+Math.random()
 	$('#shuffle').click(function() {
 		var tiles = grid.children();
 		tiles.sort(function() {
 			return 0.5 - Math.random();
 		});
-		tiles.detach().appendTo(grid);
-		msnry.layout();
-		// grid.masonry('reloadItems');
+		tiles.each(function(i) {
+			var tile = $(this);
+			tile.attr('data-id', i);
+		});
+		// tiles.detach().appendTo(grid);
+
+		$('.grid').isotope('updateSortData').isotope();
+
+		// var items = [];
+		// tiles.each(function(x) {
+		// 	items.append($('.grid').masonry('getItem', x));
+		// });
+
+		// tiles.css('left', 0);
+		// tiles.css('top', 0);
+		// msnry.reload();
+		// $('.grid').masonry('layoutItems', tiles);
+		// $('.grid').masonry('layoutItems', items);
 
 		// tiles.each(function(i) {
 		// 	var tile = $(this);
@@ -104,6 +132,14 @@ $(document).ready(function() {
 
 	$('#name').click(function() {
 		$('#info').slideToggle();
+		// var info = $('#info');
+		// if (info.css('display') === 'none') {
+		// 	$('.header').css('margin-bottom', '0px');
+		// 	info.slideDown();
+		// } else {
+		// 	$('.header').css('margin-bottom', '30px');
+		// 	info.slideUp();
+		// }
 	});
 
 	// $('.header').css('opacity', 0).delay(200).animate({ opacity: 1 }, 500);
@@ -112,28 +148,45 @@ $(document).ready(function() {
 	var showCarousel = function() {
 		dimmer.show();
 		dimmer.css('height', $(window).height());
+
+		carouselLabel.show();
+		carouselCaption.show();
 		carousel.show();
+		$('.carousel div img').css('height', 0.8 * $(window).height());
 		carousel.slick({
 			slidesToShow: 3,
 			arrows: false,
 			infinite: false,
 			centerMode: true,
-			initialSlide: 1
+			initialSlide: 0,
+			variableWidth: true
 		});		
 	}
 
-	$('#tile-pidgin').click(function() {
-		var images = [
-		"images/Pidgin/pidgin1.png",
-		"images/Pidgin/pidgin2.png",
-		"images/Pidgin/pidgin3.png",
-		"images/Pidgin/pidgin4.png",
-		"images/Pidgin/pidgin5.png",
-		"images/Pidgin/pidgin6.png",
-		"images/Pidgin/pidgin7.png"];
+	$('.tile').click(function() {
+		var label;
+		var caption;
+		var images;
+		switch ($(this).data("gallery")) {
+			case "pidgin":
+				label = "Pidgin";
+				caption = "is a graduate student in the Architecture program at Princeton. Previously, he has worked as a designer for Pentagram, 2x4, and Ateliers Jean Nouvel.";
+				images = [
+					"images/Pidgin/pidgin1.png",
+					"images/Pidgin/pidgin2.png",
+					"images/Pidgin/pidgin3.png",
+					"images/Pidgin/pidgin4.png",
+					"images/Pidgin/pidgin5.png",
+					"images/Pidgin/pidgin6.png",
+					"images/Pidgin/pidgin7.png"];
+				break;
+		}
+
 		images.forEach(function(x) {
 			carousel.append('<div class="carousel-item"><img src="'+x+'"></div>');
 		});
+		carouselLabel.text(label);
+		carouselCaption.text(caption);
 		$('.carousel-item').click(function() {
 			carousel.slick('slickGoTo', $(this).data("slick-index"));
 		});
@@ -145,6 +198,8 @@ $(document).ready(function() {
 		carousel.slick('unslick');
 		carousel.empty();
 		carousel.hide();
+		carouselLabel.hide();
+		carouselCaption.hide();
 		dimmer.hide();
 	});
 
